27373da135020a4da2db5c1a43376950eb67e732
qevolg <2227465945@qq.com>
2025-05-30 17:41:21 +0800

    fix: output tdengine query payload same with mysql

--- tdengine.js
+++ tdengine.js
@@ -3,13 +3,13 @@ module.exports = function(RED) {
     "use strict";
     var reconnect = RED.settings.tdengineReconnectTime || 20000;
     var taos      = require('@tdengine/websocket');
-    var wsSql    = null;
 
     //
     // Node
     //
 
     function TDengineNode(n) {
+        console.log("TDengineNode create.");
         RED.nodes.createNode(this, n);
         this.host = n.host;
         this.port = n.port;
@@ -20,6 +20,7 @@ module.exports = function(RED) {
         this.connecting = false;
 
         this.dbname = n.db;
+        this.wsSql  = null;
         this.setMaxListeners(0);
         var node = this;
 
@@ -34,14 +35,36 @@ module.exports = function(RED) {
         }
 
         async function doConnect() {
+            console.log("call doConnect ...");
+            console.log("host:" + node.host + " port:" + node.port + " user:" + node.credentials.user + " pass:" + node.credentials.password);
             node.connecting = true;
             node.emit("state","connecting");
-            if (!wsSql) {
+            if (!node.wsSql) {
                 let dsn = "ws://" + node.host + ":" + node.port;
                 let conf = new taos.WSConfig(dsn);
+                conf.setUser(node.credentials.user);
+                conf.setPwd(node.credentials.password);
+                conf.setDb(node.db);
                 // conn
-                wsSql = await taos.sqlConnect(conf);
-                console.log("Connected to " + dsn + " successfully.");
+                try {
+                    node.wsSql = await taos.sqlConnect(conf);
+
+                    // update status
+                    node.connected = true;
+                    node.connecting = false;
+                    node.emit("state","connected");
+                    console.log("Connected to " + dsn + " successfully!")
+                    
+                } catch (error) {
+                    node.connected = false;
+                    node.connecting = false;
+                    console.log("Connected to " + dsn + " failed! error:" + error)
+                    node.emit("state","failed to connect");
+                }
+                
+                
+            } else {
+                console.log("already is connected.")
             };
         }
 
@@ -49,19 +72,22 @@ module.exports = function(RED) {
         node.connect = function() {
             if (!node.connected && !node.connecting) {
                 doConnect();
+            } else {
+                console.log("call connect already conn.");
             }
         }
 
         // close trigger
         node.on('close', function(done) {
+            console.log("call onclose ...");
             if (node.tick) { clearTimeout(node.tick); }
             if (node.check) { clearInterval(node.check); }
             // node.connection.release();
             node.emit("state"," ");
             if (node.connected) {
                 node.connected = false;
-                if (wsSql) {
-                    wsSql.close();
+                if (node.wsSql) {
+                    node.wsSql.close();
                 }      
             }
             else {
@@ -82,18 +108,21 @@ module.exports = function(RED) {
     //
 
     function TDengineDBNodeIn(n) {
+        console.log("TDengine DBNodeIn create.")
         RED.nodes.createNode(this, n);
         this.mydb = n.mydb;
         this.mydbConfig = RED.nodes.getNode(this.mydb);
         this.status({});
 
         if (this.mydbConfig) {
+            console.log("db config is set, do connect.");
             this.mydbConfig.connect();
             var node = this;
             var busy = false;
             var status = {};
             // state
             node.mydbConfig.on("state", function(info) {
+                console.log("on state:" + info);
                 if (info === "connecting") {
                     node.status({fill: "grey", shape: "ring", text: info});
                 } else if (info === "connected") {
@@ -104,7 +133,8 @@ module.exports = function(RED) {
             });
 
             // input sql
-            node.on("input", function(msg, send, done) {
+            node.on("input",  async function(msg, send, done) {
+                console.log("on input ...");
                 send = send || function() { node.send.apply(node, arguments) };
 
                 if (node.mydbConfig.connected) {
@@ -120,17 +150,51 @@ module.exports = function(RED) {
                                 bind = msg.payload;
                             }
 
-                            conn.exec(msg.topic);
-                        };
-                    }
-                    else {
+                            console.log("call conn.query...");
+
+                            
+                            var rows = [];
+                            let i = 0;
+                            var wsRows = await conn.query(msg.topic);
+                            console.log(wsRows);
+
+                            var fields = [];
+                            var metas = await wsRows.getMeta();
+                            metas.forEach((meta, idx) => {
+                                fields.push(meta.name);
+                            });
+
+                            console.log("get fields:" + fields);
+
+
+                            while (await wsRows.next() ) {
+                                let row = wsRows.getData();
+
+                                let obj = {};
+                                fields.forEach((field, index) => {
+                                    obj[field] = row[index];
+                                });
+                                rows.push(obj);
+                                console.log('i=', i, " row obj:", obj);
+                                i += 1;
+                                if(i>1000) {
+                                    break;
+                                }
+                            }
+
+                            console.log("query end. rows count=" + i);
+                            msg.payload = rows;
+                            send(msg);
+                        } else {
+                            console.log("conn is null.")
+                        }
+                    } else {
                         if (typeof msg.topic !== 'string') {
                             node.error("msg.topic : " + RED._("tdengine.errors.notstring")); 
                             done();
                         }
                     }
-                }
-                else {
+                } else {
                     node.error(RED._("tdengine.errors.notconnected"),msg);
                     status = {
                         fill:"red",
@@ -139,14 +203,11 @@ module.exports = function(RED) {
                     };
                     if (done) { done(); }
                 }
-                if (!busy) {
-                    busy = true;
-                    node.status(status);
-                    node.tout = setTimeout(function() { busy = false; node.status(status); },500);
-                }
             });
 
+            // on close
             node.on('close', function() {
+                console.log("on close");
                 if (node.tout) { clearTimeout(node.tout); }
                 node.mydbConfig.removeAllListeners();
                 node.status({});
